26fb8803dc48555ee6fca4aa827f6a8863fd79b8
Aditya <aditya.nayak@medibuddy.in>
2026-06-20 17:22:38 +0530

    added-cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,16 @@
 import express from "express"
 import "dotenv/config"
 import connectDB from "./lib/db.js";
+import cors from "cors"
+import {clerkMiddleware } from '@clerk/express'
 
 const PORT = process.env.PORT
+FRONTEND_URL=process.env.FRONTEND_URL
 const app = express();
+
+app.use(express.json())
+app.use(cors({origin:FRONTEND_URL,credentials:true}))
+app.use(clerkMiddleware())
 
 app.listen(PORT, () => {
     connectDB();
