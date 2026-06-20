f7a288ca25577c8a1f693fd1400068354a07fcfa
Aditya <aditya.nayak@medibuddy.in>
2026-06-20 19:03:01 +0530

    docker-changes

--- server/src/server.js
+++ server/src/server.js
@@ -2,17 +2,29 @@ import express from "express"
 import "dotenv/config"
 import connectDB from "./lib/db.js";
 import cors from "cors"
-import {clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express'
+import fs from "fs";
+import path from "path";
 
-const PORT = process.env.PORT
-FRONTEND_URL=process.env.FRONTEND_URL
 const app = express();
 
+const PORT = process.env.PORT
+FRONTEND_URL = process.env.FRONTEND_URL
+
+const publicDir = path.join(process.cwd(),"public")
+
 app.use(express.json())
-app.use(cors({origin:FRONTEND_URL,credentials:true}))
+app.use(cors({ origin: FRONTEND_URL, credentials: true }))
 app.use(clerkMiddleware())
+
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir))
+    app.get("/{*any}",(req,res,next)=>{
+        res.sendFile(path.join(publicDir,"index.html"),(err)=>next(err));
+    })
+}
 
 app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on PORT : ",PORT);
+    console.log("Server is running on PORT : ", PORT);
 })
