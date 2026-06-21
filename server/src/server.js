869de650c69e771d28fd2a6dfe40cc45f7c3f609
Aditya <aditya.nayak@medibuddy.in>
2026-06-21 16:00:15 +0530

    webhook-added

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL
 
 const publicDir = path.join(process.cwd(), "public")
 
-app.use("/webhook",express.raw({type:"application/json"}),clerkWebhook)
+app.use("/webhooks",express.raw({type:"application/json"}),clerkWebhook)
 app.use(express.json())
 app.use(cors({ origin: FRONTEND_URL, credentials: true }))
 app.use(clerkMiddleware())
